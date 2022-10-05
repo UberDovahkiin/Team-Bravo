6aee3e022b25123bc04e03305bc31c5221e400d0
Nipa <Niilo.urpola@metropolia.fi>
2022-10-05 10:32:41 +0300

    Asetuksetview.java muokkausta ja väli tallennus koska file directori ei ole oikein.

--- app/src/main/java/com/example/myapplication/AsetuksetView.java
+++ app/src/main/java/com/example/myapplication/AsetuksetView.java
@@ -1,16 +1,55 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class AsetuksetView extends AppCompatActivity {
+
+    Spinner spinner;
+    EditText editTextPaino;
+    EditText editTextPituus;
+
+    public static final String MY_App = "com.example.sharedpreferences.Names";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_asetukset_view);
+
+
+        // Dropdown vaihtoehtojen luonti
+        Spinner dropdown = findViewById(R.id.spinner);
+        String[] items = new String[]{"Sukupuoli", "Mies", "Nainen", "Muu"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        dropdown.setAdapter(adapter);
+
+        editTextPituus = (EditText) findViewById(R.id.editTextPituus);
+        editTextPaino = (EditText) findViewById(R.id.editTextPaino);
+
+        Button button = (Button) findViewById(R.id.button_Save_Settings);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String pituus = editTextPituus.getText()
+                        .toString()
+                        .trim();
+
+                SharedPreferences.Editor editor = getSharedPreferences(MY_App, MODE_PRIVATE).edit();
+                editor.putString("", pituus);
+                editor.commit();
+            }
+        });
     }
 }
+
